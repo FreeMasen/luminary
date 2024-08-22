e28efd9915e7f9f34fb97cbd995d4a89c253dbe5
Robert Masen <r.f.masen@gmail.com>
2024-08-22 18:01:10 -0500

    use -l*.dll?

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
     process::{Command, Stdio},
 };
 
+use cfg_if::cfg_if;
 use clap::{Parser, ValueEnum};
 use inkwell::{
     context::Context,
@@ -232,7 +233,18 @@ fn link_exe(
     }
     #[cfg(target_os = "linux")]
     cmd.arg("-lm");
-    cmd.arg("-lluminary_runtime");
+    // #[cfg(target_os = "windows")]
+    cfg_if! {
+        if #[cfg(target_os = "windows")] {
+            if runtime_path.map(|rt| rt.join("luminary_runtime.dll.lib").exists()).unwrap_or(false) {
+                cmd.arg("-llluminary_runtime.dll")           
+            } else {
+                cmd.arg("-lluminary_runtime");
+            }
+        } else {
+            cmd.arg("-lluminary_runtime");
+        }
+    }
     let child = cmd.spawn().unwrap();
     let clang_outout = child.wait_with_output().unwrap();
     if !clang_outout.status.success() {
